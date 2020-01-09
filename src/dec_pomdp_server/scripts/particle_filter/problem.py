da9fefdd5af759c4abba21332a40a22afe1344c2
Tobi <6tkruege@informatik.uni-hamburg.de>
2020-01-09 15:09:52 +0100

    more adjusting

--- src/dec_pomdp_server/scripts/particle_filter/problem.py
+++ src/dec_pomdp_server/scripts/particle_filter/problem.py
@@ -3,7 +3,5 @@ class Problem(object):
 	def __init__(self, locations, neighbours):
 		self.locations = locations
 		self.neighbours = neighbours
-	def sample_joint_observation(self, source_xy, l0, l1):
-		pass
 	def likelihood(self, x, l0, z0):
 		pass
