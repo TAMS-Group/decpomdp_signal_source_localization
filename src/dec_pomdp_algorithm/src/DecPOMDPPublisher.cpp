427763bb56282c1733beea9620f6d85f9dcf034d
Tobi <6tkruege@informatik.uni-hamburg.de>
2020-02-19 15:32:15 +0100

    deleted unnecessary includes

--- src/dec_pomdp_algorithm/src/DecPOMDPPublisher.cpp
+++ src/dec_pomdp_algorithm/src/DecPOMDPPublisher.cpp
@@ -1,11 +1,5 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
 #include "dec_pomdp_msgs/Policy.h"
-#include <sstream>
-
-/**
- * This tutorial demonstrates simple sending of messages over the ROS system.
- */
 
 int main(int argc, char **argv)
 {
