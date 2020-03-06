0aa79544392ccf1c088ce3c18ff2c28c5f779bdd
Tobi <6tkruege@informatik.uni-hamburg.de>
2020-03-06 16:28:56 +0100

    modified deploy script to only build necessary

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ robot_user=tkrueger
 
 workspace=~/bachelor/src/
 
-rsync -ar "$workspace" $robot_user@$robot:bachelor/src && 
-ssh $robot_user@$robot -t "bash -i -c 'cd ~/bachelor && catkin build'"
+rsync -ar "$workspace" $robot_user@$robot:bachelor/src &&
+ssh $robot_user@$robot -t "bash -i -c 'cd ~/bachelor && catkin build dec_pomdp_client'"
