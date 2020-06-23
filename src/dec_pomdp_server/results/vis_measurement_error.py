0f24f7eac8bf705f490990880272422f6af768e4
16tkruege <6tkruege@informatik.uni-hamburg.de>
2020-06-23 18:34:25 +0200

    measurement error vis is now more advanced

--- src/dec_pomdp_server/results/vis_measurement_error.py
+++ src/dec_pomdp_server/results/vis_measurement_error.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
     data={}
+    colors=['b', 'g', 'r', 'y']
+    rand_colors = ['m', 'y', 'k', 'c']
     with open('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/measurement_results.json') as json_file:
         data = json.load(json_file)
     for measurements in data:
@@ -12,6 +14,15 @@ if __name__ == "__main__":
         for item in data[measurements]:
             num_of_measurements.append(item["num_measurements"])
             error.append(item["error"])
-        plt.plot(num_of_measurements, error, label=measurements)
+        line_type = '-'
+        if "Random" in measurements:
+            line_type = rand_colors.pop() + '--'
+        else:
+            line_type = colors.pop() + '-'
+
+        plt.plot(num_of_measurements, error, line_type, label=measurements)
+    plt.xlabel('number of measurements evaluated')
+    plt.xticks(np.arange(0, 401, 40))
+    plt.ylabel('Root mean squared error in m')
     plt.legend()
     plt.show()
