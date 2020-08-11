0a7a34e0a63e19e91f03371073678390a519db3a
16tkruege <6tkruege@informatik.uni-hamburg.de>
2020-08-11 22:57:26 +0200

    changed evaluation graphic slightly

--- src/dec_pomdp_server/results/rosbag_evaluation.py
+++ src/dec_pomdp_server/results/rosbag_evaluation.py
@@ -17,15 +17,15 @@ def evaluateRosbag(name):
 if __name__ == '__main__':
     rospy.init_node('rosbag_evaluation')
     # Experiment 1
-    random_error_list = evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/100RandomMovementExperiments.bag')
+    # random_error_list = evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/100RandomMovementExperiments.bag')
     # random_error_list += evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/100RandomMovementExperimentsV2.bag')
-    random_error_list += evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/100RandomMovementExperimentsV3.bag')
-    dec_pomdp_error_list = evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/50decPOMDPExperiments.bag')
-    dec_pomdp_error_list += evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/50decPOMDPExperimentsV2.bag')
-    dec_pomdp_error_list += evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/50decPOMDPExperimentsV4.bag')
-    dec_pomdp_error_list += evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/50decPOMDPExperimentsV3.bag')
-    dec_pomdp_error_list += evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/50decPOMDPExperimentsV5.bag')
-    dec_pomdp_error_list += evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/50decPOMDPExperimentsV6.bag')
+    # random_error_list += evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/100RandomMovementExperimentsV3.bag')
+    # dec_pomdp_error_list = evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/50decPOMDPExperiments.bag')
+    # dec_pomdp_error_list += evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/50decPOMDPExperimentsV2.bag')
+    # dec_pomdp_error_list += evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/50decPOMDPExperimentsV4.bag')
+    # dec_pomdp_error_list += evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/50decPOMDPExperimentsV3.bag')
+    # dec_pomdp_error_list += evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/50decPOMDPExperimentsV5.bag')
+    # dec_pomdp_error_list += evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/50decPOMDPExperimentsV6.bag')
     # additional info
     # dec_pomdp_error_list += evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/50decPOMDPExperimentsV7.bag')
     # dec_pomdp_error_list += evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/50decPOMDPExperimentsV8.bag')
@@ -34,14 +34,15 @@ if __name__ == '__main__':
     # dec_pomdp_error_list += evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/50decPOMDPExperimentsV11.bag')
     
     #Experiment 2
-    #dec_pomdp_error_list = evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/DecPOMDPNo1.bag')
-    #dec_pomdp_error_list += evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/DecPOMDPNo2.bag')
-    #random_error_list = evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/RandomMovementAttempt2-V1.bag')
+    dec_pomdp_error_list = evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/DecPOMDPNo1.bag')
+    dec_pomdp_error_list += evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/DecPOMDPNo2.bag')
+    random_error_list = evaluateRosbag('/home/tobias/Documents/Bachelor_Thesis_Code/src/dec_pomdp_server/results/RandomMovementAttempt2-V1.bag')
     data = [random_error_list, dec_pomdp_error_list]
     fig, ax = plt.subplots()
+    plt.rcParams.update({'font.size': 22})
     ax.set_title('%d Random Movement and %d dec POMDP experiments' % (len(random_error_list), len(dec_pomdp_error_list)))
     ax.set_ylabel('Weighted Root Mean Squared Error in m')
-    boxplot = ax.boxplot(data, labels=["Random Movement", "dec-POMDP"])
+    boxplot = ax.boxplot(data, labels=["Random Movement", "dec-POMDP"], widths=(0.4, 0.4))
     print('Mean of random error %f and mean of dec POMDP error %f' % (np.mean(random_error_list), np.mean(dec_pomdp_error_list)))
     print('Boxplot random error median %f and Boxplot dec-POMDP error median %f' % (boxplot['medians'][0].get_ydata()[0], boxplot['medians'][1].get_ydata()[0]))
     plt.show()
